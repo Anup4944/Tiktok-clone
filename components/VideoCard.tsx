f5cbf2b51562063c10e4e5f7af641b975afe10a8
Anup <anuppoudel60@gmail.com>
2022-07-12 12:16:26 +1000

    video component done

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 import Image from "next/Image";
 import Link from "next/Link";
 import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
-import { BsPlay, BiFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { BsPlay, BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 
 interface IProps {
@@ -12,6 +12,22 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
+  const [isHover, setIsHover] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200-pb-6">
       <div>
@@ -48,13 +64,53 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
       </div>
       <div>
         <div className="lg:ml-20 flex gap-4 relative">
-          <div className="rounded-3xl">
+          <div
+            className="rounded-3xl"
+            onMouseEnter={() => {
+              setIsHover(true);
+            }}
+            onMouseLeave={() => {
+              setIsHover(false);
+            }}
+          >
             <Link href="/">
               <video
+                ref={videoRef}
+                loop
                 className="lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
                 src={post?.video?.asset?.url}
               ></video>
             </Link>
+            {isHover && (
+              <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] p-3">
+                {playing ? (
+                  <button onClick={onVideoPress}>
+                    <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                  </button>
+                ) : (
+                  <button onClick={onVideoPress}>
+                    <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                  </button>
+                )}
+                {isVideoMuted ? (
+                  <button
+                    onClick={() => {
+                      setIsVideoMuted(false);
+                    }}
+                  >
+                    <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      setIsVideoMuted(true);
+                    }}
+                  >
+                    <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                  </button>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
